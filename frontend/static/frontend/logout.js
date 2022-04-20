4e5e4f3b3fc6b2cbab30f58fa9e9426ac41dc055
Ahmed-Nasser-99 <forall3020@gmail.com>
2022-04-20 20:05:03 +0200

    hashing the username and password and store them in the local storege

--- frontend/static/frontend/logout.js
+++ frontend/static/frontend/logout.js
@@ -1,8 +1,9 @@
 let logoutButton = document.querySelector("#logout-button");
-        logoutButton.onclick = () => {
-            fetch(`${window.location.origin}/api/logout`)
-            .then(response => response.json())
-            .then(result => {
-                window.location.replace(`${window.location.origin}`);
-            })
-        }
+logoutButton.onclick = () => {
+  fetch(`${window.location.origin}/api/logout`)
+    .then((response) => response.json())
+    .then((result) => {
+      window.localStorage.clear();
+      window.location.replace(`${window.location.origin}`);
+    });
+};
