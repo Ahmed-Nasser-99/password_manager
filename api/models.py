a44e429ec5641fdd24e24558d4fa43c445664972
Adham Salama <adhamsalama@zohomail.com>
2022-04-23 13:17:14 +0200

    Add image field to user model

--- api/models.py
+++ api/models.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    pass
+    image = models.TextField(null=False)
+
 
 class Password(models.Model):
     email = models.EmailField(null=True, blank=True)
     username = models.CharField(max_length=64, null=True, blank=True)
     encrypted_password = models.CharField(max_length=256)
     url = models.URLField(max_length=256, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='passwords')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='passwords')
     tags = models.JSONField(null=True)
